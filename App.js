37574892c8c8f5d91381a8845db1ce163593844d
KenAto <164752557+imprairies26@users.noreply.github.com>
2026-03-16 16:17:58 +0700

    Fix import path for AppNavigator in App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import AppNavigator from './src/navigation/AppNavigator';
+import AppNavigator from './src/navigation/AppNavigation';
 
 export default function App() {
   return (
